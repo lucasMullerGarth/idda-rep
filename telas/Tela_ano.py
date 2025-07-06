a38d0518a08e1f946ed084176c5e68fbf3913853
Darkcomet51 <leonardoaamuller@gmail.com>
2025-07-06 16:58:49 -0300

    bot

--- telas/Tela_ano.py
+++ telas/Tela_ano.py
@@ -19,26 +19,26 @@ class TelaAno(TelaBase):
         anchor="center"
         ).pack(pady=50)
 
-        self.criar_botao("2024", lambda: self.bot_navegador(controle,200001,"Base_de_Dados_IDSC-BR_2024.xlsx")).pack(pady=30)
-        self.criar_botao("2023", lambda: self.bot_navegador(controle,200001,"Base_de_Dados_IDSC-BR_2024.xlsx")).pack(pady=30)
-        self.criar_botao("2022", lambda: self.bot_navegador(controle,200001,"Base_de_Dados_IDSC-BR_2024.xlsx")).pack(pady=30)
+        self.criar_botao("2024", lambda: self.bot_navegador(controle,"//button[text()='Baixar a base de dados do IDSC-BR 2023 ']","Base_de_Dados_IDSC-BR_2024.xlsx")).pack(pady=30)
+        self.criar_botao("2023", lambda: self.bot_navegador(controle,"//button[text()='Baixar a base de dados do IDSC-BR 2023 ']","Base_de_Dados_IDSC-BR_2023.xlsx")).pack(pady=30)
+        self.criar_botao("2022", lambda: self.bot_navegador(controle,"//button[text()='Baixar a base de dados do IDSC-BR 2022 ']","Base_de_Dados_IDSC-BR_2022.xlsx")).pack(pady=30)
     
-    def bot_navegador(self, controle, IP, nome_do_arquivo):
+    def bot_navegador(self, controle, nome, nome_do_arquivo):
         
         Bot = Bot_ODS()
         
-        if (Bot.verifica_arquivos(IP,nome_do_arquivo)):
+        if (Bot.verifica_arquivos(nome_do_arquivo)):
             
             controle.mostrar_tela("TelaODS")
             
         else:
             controle.mostrar_tela("TelaDeEspera")
-            self.after(100, lambda: self.executa_bot(Bot, controle, IP, nome_do_arquivo))
+            self.after(100, lambda: self.executa_bot(Bot, controle, nome,))
 
      
-    def executa_bot(self, Bot, controle, IP, nome_do_arquivo):
+    def executa_bot(self, Bot, controle, nome,):
         
-        Bot.baixa_arquivos(IP, nome_do_arquivo)
+        Bot.baixa_arquivos(nome)
         controle.mostrar_tela("TelaODS")
         
     
